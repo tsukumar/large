a6ab6e707c70d04e4613f79566c26c2d84d07ea1
tsukumar <30828604+tsukumar@users.noreply.github.com>
2018-02-08 14:05:45 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() 
+{
+int a,k,j,n;
+
+printf("enter the number:");
+scanf("%d",&a);
+int b[a];
+for(k=1;k<=a;k++)
+{
+scanf("%d",&b[k]);	
+}
+for(k=1;k<=a;k++)
+{
+	
+for(j=k+1;j<=a;j++)
+{
+	
+if(b[k]>b[j])
+{
+	n=b[k];
+	b[k]=b[j];
+	b[j]=n;
+}
+}
+}
+printf("\n Output\n %d",b[(a/2+1)]);
+return 0;
+
+}
